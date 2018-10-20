e1f5ec294a8695d93ecbca7285aae4f9ecd6b5f6
jGar18 <44198904+jGar18@users.noreply.github.com>
2018-10-20 17:32:13 +0100

    Updated items

---
+++ items.py
@@ -0,0 +1,118 @@
+""" This file handles item definition. An item is a dictionary with the following attributes 
+
+    "name" - string 
+    "description" - string
+
+"""
+
+dinghy = {
+
+	"name": "dinghy",
+	"description": "An inflatable pool toy with garish yellow and pink bands. Flames curl along its edges, seemingly an anathema to the rest of the design, bar the mermaid glaring acorss the open water from wehere she is chained to the prow."
+
+    }
+
+
+ass = {
+	
+	"name": "donkey",
+	"description" : "A large farm animal, probably able to bear your weight if you so wished. He will follow where you lead , in awe of your piety."
+
+}
+
+pole = {
+	
+	"name":"Umbrella",
+	"description": "A large umbrella of black cloth and cold metal. The fabric is still wet from use."
+}
+
+earplug = {
+	
+	"name": "earbuds",
+	"description" : "a set of wireless earbuds with a curious coating of the prevoius owners earwax. Yuck. You try and turn them on... but the battery is dead."
+}
+
+integrity = {
+    
+    "name": "Integrity",
+    "description" : "Your personal pride. Easy to lose."
+    
+    }
+
+boards = {
+	
+	"name": "Wooden Planks",
+	"description": """A bunch of thin wooden boards, all that remains of your 'foe'. It will only take you a short distance before the ancient wood rots and shatters."""
+
+}
+
+rope = {
+	
+	"name": "Worn Rope",
+	"description": "Wind beaten rope from the wreck of a previous vessel, despite being frayed and worn, it is still usable."
+
+}
+
+raft = {
+	
+	"name": "Homemade Raft",
+	"description": "A raft you built from gathered resources. The achievement fills you with pride, it is poorly crafted and breaks after one use."
+
+}
+
+tailbone = {
+	
+	"name": "Coccyx",
+	"description": "The tailbone from a human, there is no apparent use for it."
+
+}
+
+boatInstructions = {
+	
+	"name": "Build a boat instructions",
+	"description": "1) get wood"
+	               "2) get rope"
+	               "3) BUILD BOAT"
+
+}
+
+booster1 = {
+	
+	"name": "Strange Ring",
+	"description": "A peculiar ring made of gold, the blood red gem on the top gives off a faint glow. It makes you feel nimble."
+
+}
+
+letterL = {
+	
+	"name": "L",
+	"description": "The letter L"
+
+}
+
+Panacea = {
+	
+	"name": "Panacea",
+	"description": "A magical cure for all ailments, a gift from the sorceress."
+
+}
+
+sandwich = {
+	
+	"name": "Ham Sandwich",
+	"description": "3 succulent slices of ham with emerald green leaves of lettuce, all wrapped in soft wholegrain bread. Restores 10hp."
+
+}
+
+drink = {
+	
+	"name": "",
+	"description": ""
+
+}
+
+crisps = {
+	
+	"name": "Salted Crisps",
+	"description": "A simple packet of slated crisps, the packet assures no artificial flavourings were used. Restores 5hp."
+}

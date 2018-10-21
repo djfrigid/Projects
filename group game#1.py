332903d155b64368d47e30acedd881aa21950a1f
oliverstoch <44123144+oliverstoch@users.noreply.github.com>
2018-10-21 17:34:31 +0100

    Update group game#1.py
    #3

--- group game#1.py
+++ group game#1.py
@@ -1,4 +1,6 @@
-from items import *
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> from items import *
 
 """ This handles room definitions . A room is a dictionary with the following attributes 
 
@@ -11,6 +13,7 @@ from items import *
 """
 
 rooms = {
+	 
     calypsoCave,
     beach,
     basment,
@@ -23,10 +26,17 @@ rooms = {
     }
 
 
+
 calypsoCave={
     
     "name" : """Calypso's cave""",
-    "description": "PLACEHOLDER STRING"
+    "description": ""
+
+    'exits'{
+	'east':'Cyclops entry'
+	'down':'Basement'
+	'south':'Beach'}
+
 
 }
 	
@@ -34,28 +44,46 @@ beach={
     
     "name" : """Calypso's island""",
     "description": "PLACEHOLDER STRING"
+    'exits'{'north':'calypsoCave'}
 
 }
 	
 basment={
     
     "name" : "The Basment",
-    "description": "PLACEHOLDER STRING"
+    "description": "",
+    "items": [box],
+    'exits'{'up':'calypsoCave'}
 
 }
 	
     
 Circe={
     
-    "name" : """Circe's island""",
-    "description": "You reach a beautiful island were the witch Circe's living.Circe could turns men into pigs and to protect yourself from her enchanments you can eat her magic herbs"
+    "name" : """Circe's Mansion""",
+    "description": """The Mansion you arrive at boast grand white mabrle collumbs and suspicously coulerfull flowers placed seemingly at random around the front room. 
+    Walking down the stairs wearing nothing but a long white silk robe and thinly framedglasses is Circe.
+    Following her are three abnormaly large ginger tabby cats that lay down comptemtly by the roaring fire. Displayed in the center of the room life size bronze piggy bank.
+    """
+    
+    'examineDescription':"""Round her supple waist there is only a scarlet sash, she has a wildly seductive, crazy, loopy nymphetic air about her.
+     Smirking and addmitedly flatterd from your wondering eyes, she reminds you that hers are geographicly speaking "up here..."   """,
+
+
+
+    'exits'{'up':'treasury',
+	    'north':'ithyca',
+	    '':''}#######how does he get to island again>:?
+
+
 
 }
 	
 treasury={
     
     "name" : "Circe's Treasury",
-    "description": "PLACEHOLDER STRING"
+    "description": "PLACEHOLDER STRING",
+    'exits'{'down':'Circe'}
 
 }
 
@@ -63,53 +91,71 @@ treasury={
 
 cyclopsEntrance = {
 
-	"name" : "Professer Polys office",      #####??/???/???????//~?????/////??/??/?/??/?
-	"description": "Up a short flight of stairs,' Professer Polyphemus's Office ' reads on a semi tranclucent door in bold black gold encompassed letters. """ #this needs fixing with regard to encompassed
+	"name" : "Professer Polys office",      
+	"description": "Up a short flight of stairs,' Professer Polyphemus's Office' reads on a semi tranclucent door in bold black letters outlined in gold. """ 
         
-        ## examine function should print(The top of the doorway seems to be worn, perhaps by some unintentional headbutting. The door looks like you could open it if you so decide')
-        ####   "items":"something to awaken the poly...? "
+        "examineDescription":"""The top of the doorway seems to be worn, perhaps by some unintentional headbutting. """#######how do you know the shout command?>?/???   # "items":"something to awaken the poly...? 
 	
-"examineDescription":
+    'exits':{'south':'cyclops'
+	     'east':'''calypsoCave'''}
+
 }
 
 cyclops = {
 	
 	"name": "Cave of the Cyclops",
         
-	"description": "You enter a fluorescently lit office. Aggresive clouds of whiskey vapor arrest your sense of smell water your eyes.
-        In the center of the room is a large oak desk with hiking pole leaning on it.
+	"description": """You enter a fluorescently lit office. Aggresive clouds of whiskey vapor arrest your sense of smell cause your eyes to water.
+        In the center of the room is a large oak desk with a black umberella leaning on it.
+	The figure slumped behind it is a broad shouldered, deepcheasted, stronglimbed, freelyfreckeled, wide mouthed, longheaded, bearkneed, brawnyhanded, ruddyfaced drunkard.
+        An eyepatch clings to his skin, inset into his gaping cavernous eyesocket and A powerful rhythmic resonance of polys formidable heart thunderers rumblingly,
+        causing the ground and the lofty walls of the office to vibrate and tremble."""
+        
+        
+	"items": [umberella, earbuds, dinghy],
+	#item umberrela should be pointy and have something tieing in with theme carved on the edge of it
+                 
+        
+	"creatures" : [cyclops]  ###why is this i square braks??                                          
+"examineDescription":""
 
-        
-        The figure slumped slumbering behind it is a broad shouldered, deepcheasted, stronglimbed, freelyfreckeled, wide mouthed, longheaded, bearkneed, brawnyhanded, ruddyfaced drunkard.
-        An eyepatch clings to his skin, inset into his gaping cavernous eyesocket.
-        #A powerful rhythmic resonance formidable heart thunderer rumblingly causing the ground, vibrate and tremble and the lofty walls of the office to vibrate and tremble.", 
-        The lofty walls of the office pulse rythmically, rumbling in time with the rise and fall of Poly's chest
-        
-	"items": [umbrella, earbuds, dinghy],
-                 #perhaps umberella?
-        
-	"creatures" : [cyclops]                                              ##### add wake up squince
-"examineDescription":
+
+	    'exits':{'north':'cyclopsEntrance'}
+
 }
 
 sirenCorridor = {
     "name": "The Long Corridor",
         
-	"description": "The relentless Cardiffian rain has flooded the corridor. You see two men (one of which is oddly wearing ear muffs) steal your raft although your glad to part with it, they trying to    ",
-    #potential for sign language further hints at absence of sound
+	"description": """The relentless Cardiffian rain has flooded the corridor and only the tif taf of the water seeping throgh the cracks in the roof break the silence.
+	You can just about make out the far end where two men are wearing ear protectors.  there is an pungent smell of rotting flesh 
+	"""
         
-	"items": [],
+	"items": [redherring],
         
 	"creatures" : [],
     
-    "examineDescription": 	
+    "examineDescription":"A red herring swims in circles after its own tail."### how does this exit work if no earbuds
+    'exits':{
+	    '?/????':'sirens lair'
+	    'north':'Circe'
+	    }
 
 }
 
 sirenLair = {
 
-	"examineDescription":
-
+	 "name": "Cheerleading practice room",
+        
+	"description": """you wade into the cold water fearlesy with images of Ithica in your mind. You see girls bathing in the water red liped and doe eyed creatures dressed provocatively in blue red and white skirts.
+	The little light there reflects of firm pouting breasts and their lustrus song pollutetes your mind with desire as you paddle furiously in their direction.   """
+        
+	"items": [],
+        
+	"creatures" : [sirens],
+    
+    'exits':{'':''}   ##### if win .... how does this exit work
+	 
 }
 
 

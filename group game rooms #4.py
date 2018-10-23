60a707bf0542c2f7d017df8fd3422333afe74f59
oliverstoch <44123144+oliverstoch@users.noreply.github.com>
2018-10-23 19:21:40 +0100

    Update group game rooms #4.py
    just need to spell check and ithica

--- group game rooms #4.py
+++ group game rooms #4.py
@@ -1,5 +1,4 @@
 
-
 #Type "copyright", "credits" or "license()" for more information.
 
 
@@ -88,7 +87,7 @@ print('''Out into the rain in misrable cardiff you go after reading an artical t
 
 
 ithaca={
-  'name':'namemcnameface',
+  'name':'The leacture hall',
 
    ' description ':'place holder string',
 
@@ -98,7 +97,7 @@ ithaca={
 
     'examine Description':'place holder string',
     
-    'lowHelp' :  'place holder string',
+    'lowHelp' :  '',
     'midHelp' :  'place holder string',
     'highHelp' : 'place holder string',
 
@@ -129,11 +128,14 @@ calypsoCave={
 
     'creatures':[],
 
-    'examine Description':'place holder string',
+    'examine Description':''' Round about the cave grows luxuriant wood, alder and sweet-smelling cypress, where birds long of wing wont to nest.
+    Owls, falcons and sea-crows with chattering tongues ply their business on the sea. ''',
+
+
     
-    'lowHelp' :  'place holder string',
-    'midHelp' :  'place holder string',
-    'highHelp' : 'place holder string'
+    'lowHelp' :  'I hear the beach in horrible this time of year.',
+    'midHelp' :  'There are 3 exits from this island',
+    'highHelp' : 'Perhaps with the items you find here you will be able to escape!'
 
 
 
@@ -152,7 +154,7 @@ creating connotations of mai tais and pineapples, here it rains and thunder seem
 
 
 
-    'examineDescription':"""Time has had toll on the once great ship, it is smashed and battered. The ropes hanging from the mast are green and slippery with algae""",
+    'examineDescription':"""Time has had toll on the once great ship, it is smashed and battered. The rope hanging from the mast are green and slippery with algae""",
     
     'exits':{'north':'calypsoCave'},
 
@@ -161,9 +163,9 @@ creating connotations of mai tais and pineapples, here it rains and thunder seem
     'creatures':[],
 
     
-    'lowHelp' :  'place holder string',
-    'midHelp' :  'place holder string',
-    'highHelp' : 'place holder string'
+    'lowHelp' :  'Whatever you do, dont try and go for a swim...',
+    'midHelp' :  'What do you need from here?',
+    'highHelp' : 'The wreak has some things that could help you escape'
 
 
 }
@@ -186,11 +188,11 @@ Basement={
      
     'creatures':[box],
 
-    'examine Description':'place holder string',
+    'examine Description':'The crate seems to utter somthing about your mother under its breath. ',
     
-    'lowHelp' :  'place holder string',
-    'midHelp' :  'place holder string',
-    'highHelp' : 'place holder string'
+    'lowHelp' :  'Box',
+    'midHelp' :  'Perhaps with the items you find here you will be able to escape!',
+    'highHelp' : 'Try and think inside the box'
 
   
 
@@ -206,7 +208,7 @@ circe={
 
     "name" : """Circe's Mansion""",
 
-    "description": """The Mansion you arrive at boast grand white marble columns and suspiciously colourful flowers placed seemingly at random around the front room. 
+    "description": """The Mansion you arrive at boasts grand white marble columns and suspiciously colourful flowers placed seemingly at random around the front room. 
 
     Walking gracefully down the stairs one hand running down the balcony, wearing a long white silk robe and gold framed glasses, is Circe.
 
@@ -214,43 +216,36 @@ circe={
     terrified expression.""",
 
     
-           #'''it seems you have enraged the great posiden,
-          #  now there is now place for you to hide in,
-         #  give this to the man whose eye you poked out,
-          # he will no doubt forget what he was angry about,
-         #  and the way to your world will be safe.... iden
-        
-           # '''
-       
-    #Panacea
 
-    
 
     'examineDescription' :"""Round her supple waist is scarlet sash , she has a wildly seductive, crazy, loopy nymphetic air about her.
 
-     Smirking and visably flatterd by your wondering eyes, she reminds you that her eyes, "are up here..."   """,
-
-
+     Smirking and visably flatterd by your wondering eyes, she reminds you that her eyes, "are up here..."
+     
+                "It seems you have enraged the great posiden,
+            now there is now place for you to hide in,
+           give this to the man whose eye you poked out,
+           he will no doubt forget what he was angry about,
+          and the way to your world will be safe... iden"
+          
+          you are impressed by her eloquent lymeric and she  passes the Panacea to you.
+  
+          """,
 
     'exits':{'up':'treasury',
 
-	    'north':'ithyca',
-
-	},
-    #######how does he get to island again>:?
-
-
-
+	    'north':'ithyca',},
+    
+                                                                                               #######how does he get to island again>:?
 
     'items':[],
 
     'creatures':[],
 
-    'examine Description':'place holder string',
     
-    'lowHelp' :  'place holder string',
-    'midHelp' :  'place holder string',
-    'highHelp' : 'place holder string'
+    'lowHelp' :  'Perhaps command would ferther you in your cause.',
+    'midHelp' :  'Could this be anymore obvious?',
+    'highHelp' : 'Dont play inocent man  we know whos poked his eye out.'
 
 }
 
@@ -276,9 +271,9 @@ There is a gold door, the door doesn’t have a lock.""",
     'creatures':[],
 
     
-    'lowHelp' :  'place holder string',
-    'midHelp' :  'place holder string',
-    'highHelp' : 'place holder string',
+    'lowHelp' :  'There are only 9999 options you have to go throgh really... or perhaps its hidden somewhere and you missed it. ',
+    'midHelp' :  'Lets just say the images of the rooms wernt added because of the impressive graphics',
+    'highHelp':  'The code is hidden in the rooms, but you must look carfully',
 
 
 }
@@ -304,9 +299,9 @@ cyclopsEntrance = {
 
     'creatures':[],
 
-    'lowHelp' :  'place holder string',
-    'midHelp' :  'place holder string',
-    'highHelp' : 'place holder string'
+    'lowHelp' :  'What command could help you now?',
+    'midHelp' :  'open sesime would proberly work if you said it load enogh',
+    'highHelp' : 'Try and twist and shout, just without the twist'
 
 }
 
@@ -339,11 +334,11 @@ cyclops = {
         
 
 
-    'examine Description':'place holder string',
+    'examine Description':'Me want you is go get me my glass eye is lost',
     
-    'lowHelp' :  'place holder string',
-    'midHelp' :  'place holder string',
-    'highHelp' : 'place holder string',
+    'lowHelp' :  'claptomania migh be bad but hey you need to get to you lecture.',
+    'midHelp' :  'did you check your inventory?',
+    'highHelp' : 'it not raining but that umberella sure would help',
 
 
 
@@ -357,18 +352,11 @@ sirenCorridor = {
 
         
 
-	"description": """The relentless Cardiffian rain has flooded the corridor and only the tip tap of the water seeping through the cracks in the roof break the silence.
+    "description": """The relentless Cardiffian rain has flooded the corridor and only the tip tap of the water seeping through the cracks in the roof break the silence.
+        You can just about make out the far end where two men are wearing ear protectors. There is an pungent smell of rotting flesh seemingly coming from the depths of the water.""",
 
-                        You can just about make out the far end where two men are wearing ear protectors.
-                        There is an pungent smell of rotting flesh seemingly coming from the depths of the water.
-
-                            """,
-
-	"items": [redherring],
-
-        
-
-	"creatures" : [],
+    "items": [redherring],
+    "creatures" : [],
 
     
 
@@ -382,9 +370,9 @@ sirenCorridor = {
     
 
     
-    'lowHelp' :  'place holder string',
-    'midHelp' :  'place holder string',
-    'highHelp' : 'place holder string'
+    'lowHelp' :   'You need to cross these waters to get to Ithaca',
+    'midHelp' : 'What did you steal from Poly that could help you cross?',
+    'highHelp' : 'Proberly wise to follow the two mens example, and that dinghy will take you over the waters'
 
 
 }
@@ -402,44 +390,40 @@ sirenLair = {
 	"description": """You wade into the cold water fearlesy with images of Ithica in your mind. You see girls bathing in the water red liped and doe eyed creatures dressed provocatively in blue red and white skirts.
 
 	The little light there reflects of firm pouting breasts and their lustrus song pollutetes your mind with desire as you paddle furiously in their direction.
-	The foulest stench is in the air, and they close in to seal your doom, no mere mortal can resist,  """,
-                                                                                                        # impregnating thier womb?
+	The foulest stench is in the air, danger begins to loom, for no mere mortal can resist them, and they close in to seal your doom, """,
+                                                                                                        # impregnat thier womb?
 	"items": [],
 
 	"creatures" : [sirens],
 
-      'exits':{} ,  ##### if win .... how does this exit work
-
-
-         
-
-    'examine Description':'place holder string',
+      'exits':{} ,                                                                                ##### if win .... how does this exit work
+    'examine Description':'The hot girls seem to me to be the visible personification of absolute perfection',
     
-    'lowHelp' :  'place holder string',
-    'midHelp' :  'place holder string',
-    'highHelp' : 'place holder string'
+    'lowHelp' :  'There is no escaping this fight im afraid',
+    'midHelp' :  'There is no escaping this fight im afraid',
+    'highHelp' : 'There is no escaping this fight im afraid'
 
 
 }
 kryllafight={
-    'name':'''Encounter with Kirill '''
+    'name':'''Encounter with Kirill ''',
 
    ' description ': ''' The door off the safe flings open and a whirlwind of air compleatly messing up your hairdoo. Soon the swirling wind dies down reveling a bow.''',
 
-    ## take bow should print.... ()
+    ## take bow should print.... (by taking the bow it seems you have unwittilngly challanged the mighty krilla to a fight, )
 
     'items':[],
 
     'creatures':['krylla'],
 
-    'examine Description':'place holder string',
+    'examine Description':'',
+    'exits':{},
     
-    'lowHelp' :  'place holder string',
-    'midHelp' :  'place holder string',
-    'highHelp' : 'place holder string',
+    
+    'lowHelp' :  'Try and Stay alive',
+    'midHelp' :  '''If you are feeling this is a fight you can acually win, i recomend trying to read 'The art of war' to make sure you have a chance ''',
+    'highHelp' : 'Im sorry kiddo cant help you here im afraid...',
 
-    'exits'{}
-    
     
          }
 
@@ -447,33 +431,33 @@ kryllafight={
 
 
 
-rooms = [
+rooms = {
 
 	 
-    kryllafight+,
+    'kryllafight':kryllafight,
          
-    calypsoCave,
+    'calypsoCave':calypsoCave,
 
-    beach,
+    'beach':beach,
 
-    basment,
+    'basment':basment,
 
-    cyclopsEntrance,
+    'cyclopsEntrance':cyclopsEntrance,
 
-    cyclops,
+    'cyclops':cyclops,
 
-    sirenCorridor,
+    'sirenCorridor':sirenCorridor,
 
-    sirenLair,
+   'sirenLair':sirenLair,
 
-    treasury,
+    'treasury':treasury,
 
-    circe,
+    'circe':circe,
     
-    ithaca
+    'ithaca':ithaca
 
     
-    ]
+    }
 
   
 

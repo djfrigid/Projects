4580e13f5a94ead9e547ff8ee6a33fb7fea8f516
Mark <electromagneticsnake@hotmail.co.uk>
2018-10-24 14:32:12 +0100

    re updated take and drop

--- TextAdventure/Modules/utilities.py
+++ TextAdventure/Modules/utilities.py
@@ -109,14 +109,18 @@ def executeTake(item):
     "You cannot take that."
     """
     
-    for i in currentRoom["items"]:
-        
-        if i["id"] == item:
-            player["inventory"].append(ItemList[item])
-            currentRoom["items"].remove(ItemList[item])
-        
+    if item not in ItemList:
+        print("You cannot take that.")
+    else:
+        cur_item = ItemList[item]
+        if cur_item not in currentRoom['items']:
+            print("You cannot take that.")
         else:
-            print("You cannot take that")
+            player["inventory"].append(cur_item)
+            print("You have taken " + cur_item["name"])
+            print("Item description: " + cur_item["description"])
+            currentRoom['items'].remove(cur_item)
+    
         
 def executeDrop(item):
     
@@ -125,15 +129,16 @@ def executeDrop(item):
     no such item in the inventory, this function prints "You cannot drop that."
     """
     
-    for i in player["inventory"]:
-        
-        if item == i["id"]:
-            store = i
-            player["inventory"].remove(i)
-            currentRoom["items"].append(store)
+    if item not in ItemList:
+        print("You cannot drop that.")
+    else:
+        cur_item = ItemList[item]
+        if cur_item not in player["inventory"]:
+            print("You cannot drop that.")
         else:
-            print("You cannot drop that")
-            
+            print("You have dropped " + cur_item['name'])
+            currentRoom['items'].append(cur_item)
+            player["inventory"].remove(cur_item)
     
    
 def isValidExit(exits, exitChoice):
